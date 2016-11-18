80e8f836673ddc7db592be7fa752aa51f12d1854
showmsg <showmsg@163.com>
2016-11-18 14:58:28 +0800

    1.增加策略报单处理说明

--- src/business/plot/plotbase.cpp
+++ src/business/plot/plotbase.cpp
@@ -18,16 +18,19 @@ bool CPlotBase::SendOrder(Json::Value root)
 //订单响应，CTP一般只有出错才会返回响应
 bool CPlotBase::OnRspOrderInsert(Json::Value root)
 {
-
 	LOG_INFO("OnRspOrderInsert %s", root["ErrorMsg"].asString().c_str());
-
+	
+	///处理因报单失败促发的策略操作
+	
 }
 //撤单响应，CTP一般出错才会响应到Rsp
 bool CPlotBase::OnRspOrderAction(Json::Value root)
 {
 
 	LOG_INFO("OnRspOrderAction %s", root["ErrorMsg"].asString().c_str());
-
+	
+	///处理因撤单失败促发的策略操作
+	
 	return true;
 }
 //报撤单响应
@@ -43,7 +46,7 @@ bool CPlotBase::OnRtnOrder(Json::Value root)
 		//未成交
 		case OS_NoTradeQueueing:
 		{
-			
+			///策略处理报单响应对应的处理，此处的成交数量不要作为最终成交，最终成交需要等收到成交后重新计算
 		}
 		break;
 		//部分成交不在队列中
@@ -55,7 +58,7 @@ bool CPlotBase::OnRtnOrder(Json::Value root)
 		//撤单
 		case OS_Canceled:
 		{
-			
+			///处理撤单响应策略
 		}
 		break;
 		default:
@@ -67,11 +70,9 @@ bool CPlotBase::OnRtnOrder(Json::Value root)
 //成交响应
 bool CPlotBase::OnRtnTrade(Json::Value root)
 {
-	
-	if(IMSG_TYPE_RTNTRADE == root["itype"].asInt())
-	{
-		LOG_INFO("RtnTrade");
-	}
+	LOG_INFO("RtnTrade");
+	///所有成交促发策略都应该在此处处理，并计算报单成交数量
+
 	return true;
 }
 		
